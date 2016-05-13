5d19b5b2be63411c72db76ff6988f7473f7877cd
sandrockman <svc-student@student.directory.jccc>
2016-05-13 14:56:52 -0500

    finished

--- Lab11-RotationalDynamics/Lab11-RotationalDynamics/Program.cs
+++ Lab11-RotationalDynamics/Lab11-RotationalDynamics/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 /**
  * @author Victor Haskins
@@ -12,6 +13,17 @@ class Program
 {
     static void Main(string[] args)
     {
+
+        StreamWriter writer1 = null;
+        string file = "oneForTheBooks.csv";
+        try
+        {
+            writer1 = new StreamWriter(File.Open(file, FileMode.OpenOrCreate));
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine("oops: "+ ex);
+        }
         //details for ball one
         double mass1 = 1.2; //in kg
         double mass2 = 4.9; //in kg
@@ -20,10 +32,11 @@ class Program
         double angRad = angle * Math.PI / 180; //in radians
 
         double timeStep = 0.1; //in seconds
+        double time = 0;
 
         Vector3D centerOfMass = new Vector3D(); // starts at zero vector
         Vector3D linearVelocity = new Vector3D();
-        double angularDisplacement = 180;
+        double angularDisplacement = 0;
         double angularVelocity = 0;
         double angularAcceleration = 0;
 
@@ -37,45 +50,45 @@ class Program
 
 
         //user supplied variables
-        Vector3D force = new Vector3D();
-        double time = 0;
         double inputTime = 0;
         Console.Write("Please enter magnitude of the Force applied: ");
         double systemForce = Convert.ToDouble(Console.ReadLine());
+
+        Vector3D forceVector = new Vector3D((systemForce * Math.Cos(angRad)),
+                                           (systemForce * Math.Sin(angRad)),
+                                            0, 1);
+
+        Vector3D accelVector = (1 / (mass1 + mass2)) & forceVector;
 
         Console.Write("Please enter time of system in seconds: ");
         inputTime = Convert.ToDouble(Console.ReadLine());
 
         while(time < inputTime)
         {
-            Console.Write("Time: " + time + "pos: (" +
-                centerOfMass.GetX() + ", " + centerOfMass.GetY() + ") vel:(");
-            Console.Write("velocity: (" +
+            
+            Console.Write("Time: " + time + ", pos: (" +
+                centerOfMass.GetX() + ", " + centerOfMass.GetY() + ")");
+            Console.WriteLine(", velocity: (" +
                 linearVelocity.GetX() + ", " + linearVelocity.GetY() + ")");
             Console.Write("and AngD: "+angularDisplacement);
-            Console.Write("AngV: " + angularVelocity);
-            Console.Write("AngA: " + angularAcceleration);
+            Console.Write(", AngV: " + angularVelocity);
+            Console.WriteLine(", AngA: " + angularAcceleration);
+            
+            writer1.WriteLine(time + ",Pos," + centerOfMass.GetX() + "," + centerOfMass.GetY() + ",Vel," + 
+                linearVelocity.GetX() + "," + linearVelocity.GetY() + ",theta," + angularDisplacement +
+                ",omega," + angularVelocity + ",alpha," + angularAcceleration);
             //Linear
             centerOfMass += linearVelocity & timeStep;
-            linearVelocity += new Vector3D((systemForce * Math.Cos(Program.D2R(angle))),
-                                           (systemForce * Math.Sin(Program.D2R(angle))),
-                                            0, 1);
+            linearVelocity += accelVector & timeStep;
             //no change to linear velocity or linear acceleration.
             //Angular
             angularDisplacement += (angularVelocity * timeStep);
             angularVelocity += (angularAcceleration * timeStep);
             //alpha = torque / moment of inertia ; torque = radius * Force * sin(theta) ; moment of inertia = (m1*r1^2 + m2*r2^2)
-            angularAcceleration = (ball1 * systemForce * Math.Sin(angle - angularDisplacement))/inertiaMoment;
+            angularAcceleration = (ball1 * systemForce * Math.Sin(angRad - angularDisplacement))/inertiaMoment;
             time += timeStep;
         }
-        Console.Write("Time: " + time + "pos: (" +
-                centerOfMass.GetX() + ", " + centerOfMass.GetY() + ") vel:(");
-        Console.Write("velocity: (" +
-            linearVelocity.GetX() + ", " + linearVelocity.GetY() + ")");
-        Console.Write("and AngD: " + angularDisplacement);
-        Console.Write("AngV: " + angularVelocity);
-        Console.Write("AngA: " + angularAcceleration);
-
+        writer1.Close();
     }
 
     static double D2R(double degrees)
